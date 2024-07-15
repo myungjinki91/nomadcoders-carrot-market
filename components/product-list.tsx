65dc9647ec1fd15e6ba65818fda41f19083efd65
Myung-jin Ki <myungjinki91@gmail.com>
2024-07-15 17:46:27 +0900

    10.10 Recap

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -54,10 +54,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {!isLastPage ? (
         <span
           ref={trigger}
-          style={{
-            marginTop: `${page + 1 * 900}vh`,
-          }}
-          className="mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩 중" : "Load more"}
         </span>
